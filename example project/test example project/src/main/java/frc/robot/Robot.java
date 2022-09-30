ca8d0895ff05441015d6b24a0dcb2bae4b33db2c
Nayan Smuek <sanjanksen4@gmail.com>
2022-09-30 11:18:42 -0700

    add test project with test in it

---
+++ example project/test example project/src/main/java/frc/robot/Robot.java
@@ -0,0 +1,11 @@
+package frc.robot;
+
+import edu.wpi.first.wpilibj.TimedRobot;
+
+public class Robot extends TimedRobot {
+
+  public static String identity(String input) {
+    return input;
+  }
+
+}
---
+++ example project/test example project/src/test/java/frc/robot/TestIdentity.java
@@ -0,0 +1,10 @@
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+import frc.robot.Robot;
+
+public class TestIdentity {
+    @Test
+    public void testOne() {
+        assertEquals("DVS.TPW-1", Robot.identity("DVS.TPW-1"));
+    }
+}
