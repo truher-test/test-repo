20645b850f2f3b7a628bac87053fa03168d51aac
Tai Vutam <809841@seq.org>
2022-09-30 11:38:46 -0700

    commit from pgm tpt 1

--- example project/test example project/src/test/java/frc/robot/TestIdentity.java
+++ example project/test example project/src/test/java/frc/robot/TestIdentity.java
@@ -6,5 +6,6 @@ public class TestIdentity {
     @Test
     public void testOne() {
         assertEquals("DVS.TPW-1", Robot.identity("DVS.TPW-1"));
+        assertEquals("PGM.TPT-1", Robot.identity("PGM.TPT-1"));
     }
 }
