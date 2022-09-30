267fa5a0abe49ca81c78875d535ae91dec2cb5c0
joels test <joel.truher@gmail.com>
2022-09-30 11:45:10 -0700

    commit from tp20

--- example project/test example project/src/test/java/frc/robot/TestIdentity.java
+++ example project/test example project/src/test/java/frc/robot/TestIdentity.java
@@ -6,5 +6,6 @@ public class TestIdentity {
     @Test
     public void testOne() {
         assertEquals("DVS.TPW-1", Robot.identity("DVS.TPW-1"));
+        assertEquals("TP-20", Robot.identity("TP-20"));
     }
 }
