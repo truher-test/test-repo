4be93278c68412d94c90c58b308566bf60ceff7e
795327 <795327@seq.org>
2022-09-30 11:43:02 -0700

    verified java on pgm dlp 3

--- example project/test example project/src/test/java/frc/robot/TestIdentity.java
+++ example project/test example project/src/test/java/frc/robot/TestIdentity.java
@@ -6,5 +6,6 @@ public class TestIdentity {
     @Test
     public void testOne() {
         assertEquals("DVS.TPW-1", Robot.identity("DVS.TPW-1"));
+        assertEquals("PGM.DLP-3", Robot.identity("PGM.DLP-3"));
     }
 }
